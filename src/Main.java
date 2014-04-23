dd5997f674ac8e48100bb9f3ea2113c751565599
chriskurn <chris@kurnmail.com>
2014-04-23 12:40:00 -0500

    the files

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+/*
+   	Description: 	Object-Oriented Software Development
+   					Quarter Programming Project
+	Authors:  		Chris Kurn and Patrick Stein
+	Class:			SE-350
+	Date:			Spring Quarter 2014 (04/22/14)
+ */
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Start");
+
+
+	}
+
+}
---
+++ src/Mover.java
@@ -0,0 +1,13 @@
+/*
+   	Description: 	Object-Oriented Software Development
+   					Quarter Programming Project
+	Authors:  		Chris Kurn and Patrick Stein
+	Class:			SE-350
+	Date:			Spring Quarter 2014
+ */
+
+
+public interface Mover {
+
+}
+
