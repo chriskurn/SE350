fe5a5f85437fcce5c12ecbcf75effff2ed34ccd8
chriskurn <chris@kurnmail.com>
2014-05-05 20:43:30 -0500

    ck

--- src/elevator/movement/ElevatorMoveUpDown.java
+++ src/elevator/movement/ElevatorMoveUpDown.java
@@ -10,40 +10,37 @@ package elevator.movement;
 
 public class ElevatorMoveUpDown {
 
-    public ElevatorMoveUpDown() {}
+    public ElevatorMoveUpDown() {
+    	
+    	
+    }
 
 	public void moveToFloor() {
-	    int newFloor;
-	    int maxFloor = 10;	//should be set by the xml file    
-	    int minFloor = 0;	//should be set by the xml file
-	    int idleFloor = (maxFloor-minFloor)/2;
-	    int direction;
-	    //Simulator.getInstance().getElevatorInformation()
-	    //Elevator.numberOfSecondsPerFloor
-	    //Floor request comes from elevatorController.java
+	    //int newFloor;
+	    //int maxFloor = 10;	//should be set by the xml file    
+	    //int minFloor = 0;	//should be set by the xml file
+	    //int idleFloor = (maxFloor-minFloor)/2;
+	    //int direction;
+     
+	    SimulationInformation elevatorInfo = Simulator.getInstance().getSimulationInfo();
+	
+	    /**
+	     * This method will move the elevator towards a destination.
+	     */
+	    public void move(){
+	        //Stub for move method
+	        System.out.println("I am moving.");
+	        try {
+	            Thread.sleep(elevatorInfo.floorTime);
+	        } catch (InterruptedException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	        System.out.println("I am done moving.");
+	    }
 	    
-	    sleep(timeForEachFloor);
-	
-	    System.out.println("Enter your destination floor> ");
-	    moveToFloor = readInScreen.nextInt();
-	    if (newFloor > maxFloor || newFloor < minFloor) {
-	        System.out.println("Invalid selection");
-	    }
-	
-	    else if (newFloor <= maxFloor && newFloor > minFloor) {
-	        for (int i = 1; i <= moveToFloor; i++)
-	            System.out.println("Floor: " + i);
-	            //Illuminate floor light if available
-	            elevatorIdlePosition(moveToFloor);
-	    }
-	}
-	
-	
-	public void elevatorIdlePosition(int idleFloor){
-	    for (int i=idleFloor; i>0;i--){
-	         System.out.println("Floor: " + i);
-	    }
-	    System.out.println("Elevator at idle floor");
+	    
+
 	
 	}
 }
