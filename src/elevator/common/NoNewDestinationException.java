0cf8b8b78dd73218e5f1efb7848298a275832c32
chriskurn <chris@kurnmail.com>
2014-05-07 15:49:17 -0500

    bump

--- src/elevator/common/NoNewDestinationException.java
+++ src/elevator/common/NoNewDestinationException.java
@@ -1,5 +1,13 @@
 package elevator.common;
 
+/**
+ * Description: NoNewDestinationException class
+ * @author Patrick Stein
+ * @author Chris Kurn 
+ * @since Version 1.0 - Spring Quarter 2014
+ * @see package elevator.common
+ */
+
 public class NoNewDestinationException extends Exception {
     
     
