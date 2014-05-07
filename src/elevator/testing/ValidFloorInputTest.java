d2e764451cc3e86bb66939a4f4c6f8df2334f2d2
chriskurn <chris@kurnmail.com>
2014-05-07 15:45:29 -0500

    missed one

--- src/elevator/testing/ValidFloorInputTest.java
+++ src/elevator/testing/ValidFloorInputTest.java
@@ -1,11 +1,4 @@
 package elevator.testing;
-/**
- * Description: validFloorInputTest class
- * @author Patrick Stein
- * @author Chris Kurn 
- * @since Version 1.0 - Spring Quarter 2014
- * @see package elevator.testing
- */
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,6 +16,25 @@ import simulator.Simulator;
 import simulator.common.IllegalParamException;
 import simulator.common.NullFileException;
 import simulator.common.SimulationInformation;
+
+/**
+ * Description: validFloorInputTest class
+ * @author Patrick Stein
+ * @author Chris Kurn 
+ * @since Version 1.0 - Spring Quarter 2014
+ * @see package elevator.testing
+ * @see import static org.junit.Assert.assertEquals;
+ * @see import org.junit.Before;
+ * @see import org.junit.Test;
+ * @see import elevator.common.Elevator;
+ * @see import elevator.common.ElevatorImpl;
+ * @see import elevator.common.InvalidFloorException;
+ * @see import elevator.common.NoNewDestinationException;
+ * @see import simulator.Simulator;
+ * @see import simulator.common.IllegalParamException;
+ * @see import simulator.common.NullFileException;
+ * @see import simulator.common.SimulationInformation;
+ */
 
 public class ValidFloorInputTest {
     
