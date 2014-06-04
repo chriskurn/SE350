48067b0ec2e65793bec889eb49ef9c81d06899cf
chriskurn <chris@kurnmail.com>
2014-06-04 18:00:01 -0500

    added a few more tests

---
+++ src/simulator/testing/ElevatorSleepTimeTest.java
@@ -0,0 +1,44 @@
+package simulator.testing;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.junit.Test;
+import simulator.Simulator;
+import simulator.common.IllegalParamException;
+import simulator.common.NullFileException;
+import simulator.common.SimulationInformation;
+
+/**
+ * Description: Express Elevator Test.
+ * 
+ * Test for an express elevator in the simulation.
+ *
+ * @author Patrick Stein
+ * @author Chris Kurn
+ * @since Version 1.0 - Spring Quarter 2014
+ */
+
+public class ElevatorSleepTimeTest {
+
+	@Test
+	public void Test() {
+
+        try {
+            Simulator.getInstance().buildSimulator("simInput.properties");
+        } catch (NullFileException | IllegalParamException | IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        Simulator.getInstance().getSimulationInfo();
+        SimulationInformation info = Simulator.getInstance()
+                .getSimulationInfo();
+
+        long sleepTime = info.elevatorSleepTime;
+        
+        if(sleepTime == 15000){
+        	System.out.println("Elevator sleep time = " + sleepTime);
+        } else fail("simInput error - The elevator sleep time for the sim is out of spec.");
+	}
+
+}
---
+++ src/simulator/testing/FloorTimeTest.java
@@ -0,0 +1,44 @@
+package simulator.testing;
+
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.junit.Test;
+import simulator.Simulator;
+import simulator.common.IllegalParamException;
+import simulator.common.NullFileException;
+import simulator.common.SimulationInformation;
+
+/**
+ * Description: Floor time input value Test.
+ * 
+ * Test for the default floor time value.
+ *
+ * @author Patrick Stein
+ * @author Chris Kurn
+ * @since Version 1.0 - Spring Quarter 2014
+ */
+
+public class FloorTimeTest {
+
+	@Test
+	public void Test() {
+
+        try {
+            Simulator.getInstance().buildSimulator("simInput.properties");
+        } catch (NullFileException | IllegalParamException | IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        Simulator.getInstance().getSimulationInfo();
+        SimulationInformation info = Simulator.getInstance()
+                .getSimulationInfo();
+
+        long floorTime = info.floorTime;
+        
+        if(floorTime == 500){
+        	System.out.println("Door Time = " + floorTime);
+        } else fail("simInput error - Floor Time should be 500");
+	}
+
+}
