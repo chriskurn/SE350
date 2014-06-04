f906644f203f2717276afe5bbb39a57acc864224
PStein4 <PSTEIN4@mail.depaul.edu>
2014-06-04 18:07:13 -0500

    Added one new test for elevator controller

---
+++ src/elevator/testing/ElevatorControllerTest.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package elevator.testing;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import simulator.Simulator;
+import simulator.common.IllegalParamException;
+import simulator.common.NullFileException;
+import simulator.common.SimulationInformation;
+import elevator.common.ElevatorDirection;
+import elevator.control.ElevatorController;
+import elevator.control.ElevatorRequestHandler;
+import elevator.control.ElevatorRequestHandlerFactory;
+import elevator.elements.Elevator;
+import elevator.elements.ElevatorImpl;
+
+/**
+ * @author Patrick Stein
+ *
+ */
+public class ElevatorControllerTest {
+    private SimulationInformation info;
+
+    /** The sim. */
+    private Simulator sim;
+
+    /** The floor number. */
+    private int floorNumber;
+
+    /** The elevator. */
+    private ArrayList<Elevator> eles = new ArrayList<Elevator>();
+    
+    ElevatorController ec;
+
+    /**
+     * Load simulation.
+     * 
+     * @throws NullFileException
+     *             the null file exception
+     * @throws IllegalParamException
+     *             the illegal param exception
+     * @throws IOException
+     *             Signals that an I/O exception has occurred.
+     */
+    @Before
+    public void loadSimulation() throws NullFileException,
+            IllegalParamException, IOException {
+        Simulator mySim = Simulator.getInstance();
+        mySim.buildSimulator("simInput.properties");
+        this.info = mySim.getSimulationInfo();
+        this.sim = mySim;
+        ec = ElevatorController.getInstance();
+        ec.stopAllElevators();
+    }
+    
+    @Test
+    public void validRequestAddTest() throws IllegalParamException{
+        ec.addNewRequest(5, ElevatorDirection.UP);
+    }
+    
+    @Test(expected = IllegalParamException.class)
+    public void invalidDirectionRequestTest() throws IllegalParamException{
+        ec.addNewRequest(5, ElevatorDirection.IDLE);
+    }
+    
+    @Test(expected = IllegalParamException.class)
+    public void invalidUpperFloorRequestTest() throws IllegalParamException{
+        ec.addNewRequest(30, ElevatorDirection.UP);
+    }
+    @Test(expected = IllegalParamException.class)
+    public void invalidLowerFloorRequestTest() throws IllegalParamException{
+        ec.addNewRequest(-5, ElevatorDirection.DOWN);
+    }
+    
+
+}
